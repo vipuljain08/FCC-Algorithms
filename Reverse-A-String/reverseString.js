07ac0c7d657cc5d9bcf422eb5b795cdbdf6309b3
Vipul Jain <vupuljain2001@gmail.com>
2019-01-17 09:47:33 +0530

    create reverseStr.js

---
+++ Reverse-A-String/reverseString.js
@@ -0,0 +1,8 @@
+// -----------------------------------
+
+// Reverse A String
+
+const reverseStr = str => str.split(['']).reverse().join('')
+
+reverseStr("fire dragons"))  // snogard erif
+reverseStr("freecodecamp community")  // ytinummoc pmacedoceerf
