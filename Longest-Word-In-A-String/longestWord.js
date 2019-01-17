f1caea5d7e6ac98fa76c5def1b6b53de58ec70e0
Vipul Jain <vupuljain2001@gmail.com>
2019-01-17 09:38:30 +0530

    add new line

--- Longest-Word-In-A-String/longestWord.js
+++ Longest-Word-In-A-String/longestWord.js
@@ -6,3 +6,5 @@ const longestWord = str => Math.max(...str.split(' ').map(a => a.length ))
 
 longestWord("The quick brown fox jumped over the lazy dog")  // 6
 longestWord("Support an open future")  // 7
+
+
