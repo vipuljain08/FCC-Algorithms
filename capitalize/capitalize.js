ac9ed519102c767bcbc4ecb763dcf2164d4dc0b7
Vipul Jain <vupuljain2001@gmail.com>
2019-01-17 09:20:58 +0530

    Create capitalize.js

---
+++ capitalize/capitalize.js
@@ -0,0 +1,6 @@
+// capitalize single word
+const capitalize = str => str[0].toUpperCase() + str.substring(1).toLowerCase()
+
+capitalize("hello")   // Hello
+capitalize("aWESOME")   // Awesome
+capitalize("fREEcoDEcaMP")  // Freecodecamp
