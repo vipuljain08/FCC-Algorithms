27ca231585262d445ee8162b6f59844eb63e6168
Prafull Pandey <prafullpandey68@gmail.com>
2019-10-09 21:26:47 -0700

    Changes in reverse.js

--- Reverse-A-String/reverseString.js
+++ Reverse-A-String/reverseString.js
@@ -7,4 +7,4 @@ const reverseStr = str => str.split(['']).reverse().join('')
 reverseStr("fire dragons"))  // snogard erif
 reverseStr("freecodecamp community")  // ytinummoc pmacedoceerf
 
-
+// reverse a string using es6 functions
